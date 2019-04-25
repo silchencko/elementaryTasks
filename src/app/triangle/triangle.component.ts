3781cea95ddd47c14475249bc54c847583f191bf
Kateryna Maistrenko <kmais@softserveinc.com>
2019-04-25 17:23:56 +0300

    Moved validation to task-solution component for task 1,2,3,4

--- src/app/triangle/triangle.component.ts
+++ src/app/triangle/triangle.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { TrianglesService } from '../services/triangles.service';
 
 @Component({
